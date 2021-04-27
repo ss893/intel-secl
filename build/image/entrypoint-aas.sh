288de63941b0ef8002dc22eaa58bd3be16b48a66
Rashmi Rajashekar <rashmi.rajashekara@intel.com>
2021-04-27 03:43:44 -0700

    ISECL-15473: restore config file if any of setup task fails in container deployment

--- build/image/entrypoint-aas.sh
+++ build/image/entrypoint-aas.sh
@@ -33,13 +33,16 @@ if [ ! -f $CONFIG_PATH/.setup_done ]; then
 fi
 
 if [ ! -z $SETUP_TASK ]; then
+  cp $CONFIG_PATH/config.yml /tmp/config.yml
   IFS=',' read -ra ADDR <<<"$SETUP_TASK"
   for task in "${ADDR[@]}"; do
     authservice setup $task --force
     if [ $? -ne 0 ]; then
+      cp /tmp/config.yml $CONFIG_PATH/config.yml
       exit 1
     fi
   done
+  rm -rf /tmp/config.yml
 fi
 
 authservice run
--- build/image/entrypoint-cms.sh
+++ build/image/entrypoint-cms.sh
@@ -25,13 +25,16 @@ if [ ! -f $CONFIG_PATH/.setup_done ]; then
 fi
 
 if [ ! -z $SETUP_TASK ]; then
+  cp $CONFIG_PATH/config.yml /tmp/config.yml
   IFS=',' read -ra ADDR <<< "$SETUP_TASK"
   for task in "${ADDR[@]}"; do
     cms setup $task --force
     if [ $? -ne 0 ]; then
+      cp /tmp/config.yml $CONFIG_PATH/config.yml
       exit 1
     fi
   done
+  rm -rf /tmp/config.yml
 fi
 
 cms run
