ea242f5d845401c1b6a2267dbc2fd11935543f9c
R, Praveenraj <praveenraj.r@intel.com>
2021-05-27 19:56:47 +0530

    k8s deployment changes for outbound model

--- build/image/entrypoint-aas.sh
+++ build/image/entrypoint-aas.sh
@@ -14,9 +14,11 @@ CONFIG_PATH=/etc/$COMPONENT_NAME/
 CERTS_PATH=$CONFIG_PATH/certs
 CERTDIR_TOKENSIGN=$CERTS_PATH/tokensign
 CERTDIR_TRUSTEDJWTCAS=$CERTS_PATH/trustedca
+NATS_DIR_PATH=$CONFIG_PATH/nats
+NATS_NKEYS_DIR_PATH=$NATS_DIR_PATH/nkeys
 
 if [ ! -f $CONFIG_PATH/.setup_done ]; then
-  for directory in $LOG_PATH $CONFIG_PATH $CERTS_PATH $CERTDIR_TOKENSIGN $CERTDIR_TRUSTEDJWTCAS; do
+  for directory in $LOG_PATH $CONFIG_PATH $CERTS_PATH $CERTDIR_TOKENSIGN $CERTDIR_TRUSTEDJWTCAS $NATS_DIR_PATH $NATS_NKEYS_DIR_PAT; do
     mkdir -p $directory
     if [ $? -ne 0 ]; then
       echo "Cannot create directory: $directory"
