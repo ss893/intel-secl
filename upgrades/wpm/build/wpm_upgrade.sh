56d57db5f8ed7ee8f1ba5986c165d977c137ecd8
R, Praveenraj <praveenraj.r@intel.com>
2021-06-10 03:36:47 -0700

    removed secure docker daemon related code

--- upgrades/wpm/build/wpm_upgrade.sh
+++ upgrades/wpm/build/wpm_upgrade.sh
@@ -8,9 +8,6 @@ INSTALLED_EXEC_PATH="/opt/$NEW_EXEC_NAME/bin/$NEW_EXEC_NAME"
 CONFIG_PATH="/etc/$NEW_EXEC_NAME"
 echo "" >$LOG_FILE
 
-if [ -d "/opt/workload-policy-manager/secure-docker-daemon" ]; then
-  ./upgrade-secure-docker-daemon.sh |& tee -a $LOG_FILE
-fi
-
 ./upgrade.sh -v $CURRENT_VERSION -e $INSTALLED_EXEC_PATH -c $CONFIG_PATH -n $NEW_EXEC_NAME -b $BACKUP_PATH |& tee -a $LOG_FILE
+
 exit ${PIPESTATUS[0]}
