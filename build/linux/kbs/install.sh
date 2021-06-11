24001e14481e82343cb5d0b35346a0d649f5db88
Dhawale, Dinkar <dinkar.dhawale@intel.com>
2021-06-10 22:54:35 -0700

    add check for upgrade for user

--- build/linux/kbs/install.sh
+++ build/linux/kbs/install.sh
@@ -12,9 +12,23 @@ SERVICE_ENV=kbs.env
 
 # Upgrade if component is already installed
 if command -v $COMPONENT_NAME &>/dev/null; then
-  echo "$COMPONENT_NAME is installed, proceeding with the upgrade"
-  ./${COMPONENT_NAME}_upgrade.sh
-  exit $?
+  n=0
+  until [ "$n" -ge 3 ]
+  do
+  echo "$COMPONENT_NAME is already installed, Do you want to proceed with the upgrade? [y/n]"
+  read UPGRADE_NEEDED
+  if [ $UPGRADE_NEEDED == "y" ] || [ $UPGRADE_NEEDED == "Y" ] ; then
+    echo "Proceeding with the upgrade.."
+    ./${COMPONENT_NAME}_upgrade.sh
+    exit $?
+  elif [ $UPGRADE_NEEDED == "n" ] || [ $UPGRADE_NEEDED == "N" ] ; then
+    echo "Exiting the installation.."
+    exit 0
+  fi
+  n=$((n+1))
+  done
+  echo "Exiting the installation.."
+  exit 0
 fi
 
 # find .env file
